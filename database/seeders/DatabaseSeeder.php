09cc11f5bdd90614a7dd7c8243e5ecd74ae9e523
jorleytavares <tavaresjorley@gmail.com>
2026-02-10 23:02:43 -0400

    chore: adiciona BlogSeeder ao DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProblemaSeeder::class,
             TransportadoraSeeder::class,
+            BlogSeeder::class,
         ]);
     }
 }
